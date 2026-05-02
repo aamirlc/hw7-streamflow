638fd60ee01e51ff78f6313d879fea6542a408dd
Aamir Lamichhane <aamirlc@arizona.edu>
2026-05-02 14:15:16 -0700

    simplify run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-# run.sh  --  Verde River streamflow forecasting workflow
-# Edit the CONFIG section in run_forecast.py first, then run this script.
-#
-# Usage:
-#   bash run.sh
-
-set -e
-
-echo "Running HW7 streamflow forecast..."
 python run_forecast.py
